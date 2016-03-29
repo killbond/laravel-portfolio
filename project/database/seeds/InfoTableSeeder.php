273be034cefa614034892c7f470458198d108935
killbond <killbond@mail.ru>
2016-03-29 08:57:04 +0300

    phone format

--- project/database/seeds/InfoTableSeeder.php
+++ project/database/seeds/InfoTableSeeder.php
@@ -11,7 +11,7 @@ class InfoTableSeeder extends Seeder
     {
         $table = $this->table;
         DB::insert("insert into {$table} (alias, info) values (?, ?)", ['NAME', 'Talanin Andrew']);
-        DB::insert("insert into {$table} (alias, info) values (?, ?)", ['PHONE', '+7 (902) 306-42-79']);
+        DB::insert("insert into {$table} (alias, info) values (?, ?)", ['PHONE', '+7-902-306-42-79']);
         DB::insert("insert into {$table} (alias, info) values (?, ?)", ['EMAIL', 'killbond@mail.ru']);
         DB::insert("insert into {$table} (alias, info) values (?, ?)", ['PHOTO_URL', 'https://pp.vk.me/c617119/v617119671/15fce/84a55OwJ6wo.jpg']);
     }
